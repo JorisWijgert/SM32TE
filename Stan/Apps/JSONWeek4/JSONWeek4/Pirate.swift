bf6fcbc06d27e7fe022916f121ca4e9e8462cdf7
StanWulms <fhict@MacBook-Pro.local>
2016-03-10 09:43:12 +0100

    #Stan - Assignment 4 added all files
    All the file are added, not yet have an implementation.

---
+++ Stan/Apps/JSONWeek4/JSONWeek4/Pirate.swift
@@ -0,0 +1,12 @@
+//
+//  Pirate.swift
+//  JSONWeek4
+//
+//  Created by Fhict on 10/03/16.
+//  Copyright © 2016 Fhict. All rights reserved.
+//
+
+import Foundation
+class Pirate {
+    
+}
---
+++ Stan/Apps/JSONWeek4/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  JSONWeek4
+//
+//  Created by Fhict on 10/03/16.
+//  Copyright © 2016 Fhict. All rights reserved.
+//
+
+import Cocoa
+
+class TableViewController: UITableViewController {
+
+}
