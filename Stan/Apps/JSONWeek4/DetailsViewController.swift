d76e9c859e1682cc2976561207e246acc4e1bcff
StanWulms <fhict@MacBook-Pro.local>
2016-03-10 11:12:28 +0100

    #Stan - finished Pirate application
    U can now see the properties in the controllerView after pressing a
    pirate from the JSON

--- Stan/Apps/JSONWeek4/DetailsViewController.swift
+++ Stan/Apps/JSONWeek4/DetailsViewController.swift
@@ -11,4 +11,21 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    var selectedPirate: Pirate?
+    
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbLife: UILabel!
+    @IBOutlet weak var lbActiveYears: UILabel!
+    @IBOutlet weak var lbCountryOfBirth: UILabel!
+    @IBOutlet weak var tvComments: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.lbName.text = self.selectedPirate?.name
+        self.lbLife.text = self.selectedPirate?.life
+        self.lbActiveYears.text = self.selectedPirate?.yearsActive
+        self.lbCountryOfBirth.text = self.selectedPirate?.countryOrigin
+    }
 }
