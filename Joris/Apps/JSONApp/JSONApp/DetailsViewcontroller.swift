07fb77abb36dc2e69080833ee6f207f56d64824b
WALUIGI96 <fhict@MacBook-Pro-2.local>
2016-03-10 09:42:11 +0100

    JSON yay
    first setup

---
+++ Joris/Apps/JSONApp/JSONApp/DetailsViewcontroller.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsViewcontroller.swift
+//  JSONApp
+//
+//  Created by Fhict on 10-03-16.
+//  Copyright © 2016 Fontys. All rights reserved.
+//
+
+import Cocoa
+
+class DetailsViewcontroller: UIViewController {
+
+}
---
+++ Joris/Apps/JSONApp/JSONApp/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  JSONApp
+//
+//  Created by Fhict on 10-03-16.
+//  Copyright © 2016 Fontys. All rights reserved.
+//
+
+import Cocoa
+
+class TableViewController: UITableViewController {
+
+}
