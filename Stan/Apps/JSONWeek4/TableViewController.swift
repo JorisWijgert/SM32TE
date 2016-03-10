5ce49c236b69abb548b8d864373d39c5f1c7d7b6
StanWulms <fhict@MacBook-Pro.local>
2016-03-10 10:03:07 +0100

    #Stan - info plist and loadJSON added
    LoadJSON added to the tableViewController. Also added code into
    info.plist to make JSON work

--- Stan/Apps/JSONWeek4/TableViewController.swift
+++ Stan/Apps/JSONWeek4/TableViewController.swift
@@ -6,8 +6,30 @@
 //  Copyright © 2016 Fhict. All rights reserved.
 //
 
-import Cocoa
+//import Cocoa
 
 class TableViewController: UITableViewController {
 
+    var pirate = [Pirate]()
+    
+    func loadJsonData()
+    {
+        let url = NSURL(string: "http://athena.fhict.nl/users/i886625/pirates.json")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            do
+            {
+                if let jsonObject: AnyObject = try NSJSONSerialization.JSONObjectWithData(data!, 	   options: NSJSONReadingOptions.AllowFragments)
+                {
+                    self.parseJsonData(jsonObject)
+                }
+            }
+            catch
+            {
+                print("Error parsing JSON data")
+            }
+        }
+        dataTask.resume();
+    }
 }
