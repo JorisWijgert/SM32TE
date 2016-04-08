4dc4aa70f703b907af584b8ab9f59031559d4cb4
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-04-08 12:33:11 +0200

    location....

---
+++ EHBO/EHBOAppA/EHBOAppA/NewMessageMaker.swift
@@ -0,0 +1,26 @@
+//
+//  NewMessageMaker.swift
+//  EHBOAppA
+//
+//  Created by Fhict on 08-04-16.
+//  Copyright © 2016 Fontys. All rights reserved.
+//
+
+import UIKit
+
+class NewMessageMaker: UIViewController {
+
+    
+    @IBOutlet weak var locationString: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
