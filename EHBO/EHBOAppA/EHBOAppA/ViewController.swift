ded8044088ff70961d8ec0e5fcd56b864c050cb5
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-04-08 10:32:15 +0200

    updated gui and made the connection between login and main

--- EHBO/EHBOAppA/EHBOAppA/ViewController.swift
+++ EHBO/EHBOAppA/EHBOAppA/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Username: UITextField!
 
+    @IBOutlet weak var Password: UITextField!
+    
+    @IBOutlet weak var errorMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func logInAction(sender: AnyObject) {
+        if(Username.text!.characters.count > 3 && Password.text!.characters.count > 3){
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle:nil).instantiateViewControllerWithIdentifier("MainScreen")
+            self.presentViewController(viewController, animated: true, completion: nil)
+        }
+        else if(Username.text!.characters.count <= 3){
+            errorMessage.hidden=false
+            errorMessage.text = "Gebruikersnaam is te kort"
+        }
+        else if(Password.text!.characters.count <= 3){
+            errorMessage.hidden = false
+            errorMessage.text = "Wachtwoord is te kort"
+        }
+    }
 
 }
 
