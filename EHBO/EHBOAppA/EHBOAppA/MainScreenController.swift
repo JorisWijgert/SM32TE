71e7f7cc4123d683cce2a9cc473a3add2bddcda2
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-04-15 11:21:00 +0200

    made login complete

---
+++ EHBO/EHBOAppA/EHBOAppA/MainScreenController.swift
@@ -0,0 +1,30 @@
+//
+//  MainScreenController.swift
+//  EHBOAppA
+//
+//  Created by Fhict on 15-04-16.
+//  Copyright © 2016 Fontys. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenController: UIViewController {
+    @IBOutlet weak var welcomeMessage: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let preferences = NSUserDefaults.standardUserDefaults()
+        let currentUserKey = "currentUser"
+        if preferences.objectForKey(currentUserKey) != nil {
+            welcomeMessage.title = "Welkom \(preferences.objectForKey(currentUserKey)!)"
+        }
+    }
+    @IBAction func logOut(sender: UIBarButtonItem) {
+        let preferences = NSUserDefaults.standardUserDefaults()
+        let currentUserKey = "currentUser"
+        preferences.removeObjectForKey(currentUserKey)
+        preferences.synchronize()
+        let viewController:UIViewController = UIStoryboard(name: "Main", bundle:nil).instantiateViewControllerWithIdentifier("LoginScreen")
+        self.presentViewController(viewController, animated: true, completion: nil)
+    }
+}
