d46cc8c5daad6c9ea86dba9f5ea8fb7f478bfbf8
StanWulms <fhict@MacBook-Pro-2.local>
2016-04-21 12:43:50 +0200

    Added Task Controller

---
+++ EHBO/EHBOAppA/EHBOAppA/TaskController.swift
@@ -0,0 +1,14 @@
+//
+//  TaskController.swift
+//  AHBO
+//
+//  Created by Fhict on 21/04/16.
+//  Copyright © 2016 Fontys. All rights reserved.
+//
+
+import UIKit
+
+class TaskController: UIViewController {
+
+    @IBOutlet weak var lblInformatie: UILabel!
+}
